ab1231b5215cdf2257e10ee68ba308ed70e4ce63
Ignacio Navarro <ignacionavarr27@gmail.com>
2025-10-17 18:35:46 +0200

    Implementada persistencia

--- main.py
+++ main.py
@@ -16,25 +16,30 @@ def main():
         
         print_menu()
 
-        choice = input("Seleccione una opción: ")
+        try:
 
-        match choice:
-            case "1":
-                description = input("Ingrese la descripción de la tarea: ")
-                manager.add_task(description)
-            case "2":
-                manager.list_task()
-            case "3":
-                id = input("Ingrese el ID de la tarea a completar: ")
-                manager.complet_task(id)
-            case "4":
-                id = input("Ingrese el ID de la tarea a eliminar: ")
-                manager.delete_task(id)
-            case "5":
-                print("Saliendo del gestor de tareas. ¡Hasta luego!")
-                break
-            case _: 
-                print("Opción no válida. Por favor, intente de nuevo.")
+            choice = int(input("Seleccione una opción: "))
+
+            match choice:
+                case 1:
+                    description = input("Ingrese la descripción de la tarea: ")
+                    manager.add_task(description)
+                case 2:
+                    manager.list_task()
+                case 3:
+                    id = int(input("Ingrese el ID de la tarea a completar: "))
+                    manager.complet_task(id)
+                case 4:
+                    id = int(input("Ingrese el ID de la tarea a eliminar: "))
+                    manager.delete_task(id)
+                case 5:
+                    print("Saliendo del gestor de tareas. ¡Hasta luego!")
+                    break
+                case _: 
+                    print("Opción no válida. Por favor, intente de nuevo.")
+
+        except ValueError:
+            print("Entrada no válida. Por favor, ingrese un número válido.")
 
 if __name__ == "__main__":
     main()   
